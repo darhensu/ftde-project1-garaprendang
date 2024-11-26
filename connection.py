5290e1d6601f060560227baa5bc61a3ee97ebdab
AHMAD FAYYADH <20110001@ittelkom-pwt.ac.id>
2024-11-26 15:04:48 +0700

    added .gitignore and connection.py

---
+++ connection.py
@@ -0,0 +1,35 @@
+import os
+import json
+import psycopg2
+from sqlalchemy import create_engine
+
+# func config
+def config(connection_db):
+    path = os.getcwd()
+    with open(os.path.join(path, 'config.json')) as file:
+        conf = json.load(file)[connection_db]
+        return conf
+    
+# func get_conn
+def get_conn(conf, name_conn):
+    try:
+        conn = psycopg2.connect(
+            host=conf['host'],
+            database=conf['db'],
+            user=conf['user'],
+            password=conf['password'],
+            port=conf['port']
+        )
+        print(f"[INFO] succes connet postgres {name_conn}")
+        engine=create_engine(
+            "postgresql+psycopg2://{}:{}@{}:{}/{}".format(
+                conf['user'],
+                conf['password'],
+                conf['host'],
+                conf['port'],
+                conf['db']
+            ))
+        return conn, engine
+    except Exception as e:
+        print(f"[INFO] can't success connect to postgres {name_conn}")
+        print(str(e))
